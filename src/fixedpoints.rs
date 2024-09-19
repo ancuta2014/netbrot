24757d2ae3dd505b60bf79c053128e3a7b64c7d0
Alexandru Fikl <alexfikl@gmail.com>
2024-09-19 16:58:54 +0300

    feat: add our own little newton solver

--- src/fixedpoints.rs
+++ src/fixedpoints.rs
@@ -4,9 +4,8 @@
 use num::complex::{c64, Complex64};
 use rand::Rng;
 
-use eqsolver::multivariable::GaussNewton;
-
 use crate::netbrot::{netbrot_repeat, netbrot_repeat_prime, Matrix, Netbrot, Vector};
+use crate::newton::NewtonRhapson;
 
 /// {{{ find_unique_fixed_points
 
@@ -103,13 +102,15 @@ pub fn find_fixed_points_by_newton(
 
     let f = |z: Vector| netbrot_repeat_fp(&brot.mat, &z, brot.c, nperiod);
     let j = |z: Vector| netbrot_repeat_prime_fp(&brot.mat, &z, brot.c, nperiod);
-    let solver = GaussNewton::new(f, j);
+    let mut solver = NewtonRhapson::new(f, j);
+    solver.with_rtol(1.0e-8).with_maxit(512);
 
     for _ in 0..npoints {
         let z0 = generate_random_points_on_sphere(&mut rng, ndim, radius);
-        let result = solver.solve(z0).unwrap();
-
-        fixedpoints.push(z0);
+        match solver.solve(z0) {
+            Ok(z) => fixedpoints.push(z),
+            Err(_) => continue,
+        }
     }
 
     let indices = find_unique_fixed_points(brot, &fixedpoints, nperiod, eps);
--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ mod coeffeval;
 mod colorschemes;
 mod fixedpoints;
 mod netbrot;
+mod newton;
 mod render;
 
 use std::error::Error;
@@ -24,7 +25,7 @@ use std::time::Instant;
 
 use colorschemes::ColorType;
 use netbrot::Netbrot;
-use render::{pixel_to_point, render_orbit, render_period, RenderType};
+use render::{render_orbit, render_period, RenderType};
 
 use nalgebra::DMatrix;
 use num::complex::Complex64;
@@ -117,6 +118,8 @@ fn main() {
     let brot = Netbrot::new(&exhibit.mat, args.maxit, exhibit.escape_radius);
     println!("Escape radius {}", brot.escape_radius_squared.sqrt());
 
+    fixedpoints::find_fixed_points_by_newton(&brot, 512, 1, 1.0e-8);
+
     // Scope of slicing up `pixels` into horizontal bands.
     println!("Executing...");
     let now = Instant::now();
@@ -126,9 +129,9 @@ fn main() {
         bands.into_par_iter().for_each(|(i, band)| {
             let top = i;
             let band_bounds = (bounds.0, 1);
-            let band_upper_left = pixel_to_point(bounds, (0, top), upper_left, lower_right);
+            let band_upper_left = render::pixel_to_point(bounds, (0, top), upper_left, lower_right);
             let band_lower_right =
-                pixel_to_point(bounds, (bounds.0, top + 1), upper_left, lower_right);
+                render::pixel_to_point(bounds, (bounds.0, top + 1), upper_left, lower_right);
 
             match render_type {
                 RenderType::Orbit => render_orbit(
